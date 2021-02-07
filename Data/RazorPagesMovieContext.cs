b6e6fad59abf1a6bcf5235bb2b966d24156c43a3
Weverton procópio <wevertonprocopio690@gmail.com>
2021-02-07 19:22:26 -0300

    create folder data and file RazorPagesMovieContext

---
+++ Data/RazorPagesMovieContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+namespace RazorPagesMovie.Data
+{
+    public class RazorPagesMovieContext : DbContext
+    {
+        public RazorPagesMovieContext(DbContextOptions<RazorPagesMovieContext> options): base(options) {}
+
+        public DbSet<Movie> Movie {get; set;}
+    }
+
+}
